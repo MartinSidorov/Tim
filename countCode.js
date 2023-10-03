e021a63684f1da7e966f82c955c41acdcc5ada10
MartinSidorov <115223978+MartinSidorov@users.noreply.github.com>
2023-10-03 17:04:17 +0300

    Update countCode.js

--- countCode.js
+++ countCode.js
@@ -1,10 +1,10 @@
-function countCode(input) {
-  let count = 0;
-  const regex = /co.e/gi;
-
-  while (regex.exec(input)) {
-    count++;
-  }
-
-  return count;
+function countCode(str) {
+    const regex = /code/ig;
+    const matches = str.match(regex);
+    return matches ? matches.length : 0;
 }
+
+// Example usage
+const exampleString = "This is a code snippet. Here's more code.";
+const count = countCode(exampleString);
+console.log("Count of 'code':", count);  // Output: Count of 'code': 2
